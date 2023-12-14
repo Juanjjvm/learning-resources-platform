6d12b5dbe4745348418f7ef2791b1408778b084a
Juan Jose Vallejo Morales <jjvallejomorales@gmail.com>
2023-12-14 17:01:21 -0500

    Vista creada en el dashboard para crear recursos

---
+++ resources/js/Components/ResourceForm.vue
@@ -0,0 +1,29 @@
+<script setup>
+import axios from "axios";
+import { ref } from "vue";
+
+let title = ref("");
+let description = ref("");
+let link = ref("");
+
+function createResource() {
+    axios
+        .post("/api/resources", {
+            title: title.value,
+            description: description.value,
+            link: link.value,
+        })
+        .then((respones) => {
+            console.log(response);
+        });
+}
+</script>
+
+<template>
+    <div class="m-8">
+        <input type="text" v-model="title">
+        <input type="text" v-model="description">
+        <input type="text" v-model="link">
+        <button @click="createResource">Crear recurso</button>
+    </div>
+</template>
